22bb23204fa33907c93ec2e1d6003a833790391a
david052090 <barriosdavid310@gmil.com>
2021-08-24 00:00:49 -0500

    feat: segundo cambio

--- repositorio_java/src/App.java
+++ repositorio_java/src/App.java
@@ -1,6 +1,22 @@
+
+import java.util.Scanner;
+
 public class App {
+    public static Scanner ingresoTeclado;
+    public static Double[] notas;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hola, como estan?");
+        ingresoTeclado = new Scanner(System.in);
+        notas = new Double[5];
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese las notas");
+            notas[i] = ingresoTeclado.nextDouble();
+
+        }
+        for (int j = 0; j < 5; j++) {
+            System.out.println("Esta es la nota de la posición " + j + " y su nota es " + notas[j]);
+
+        }
+
     }
 }
